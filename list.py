066ba8fb22b1710825287cb7c37f5eb6bb20bd8c
Karina <kidiaievakarina@gmail.com>
2024-01-19 19:25:13 +0200

    ex-1

--- list.py
+++ list.py
@@ -1,7 +1,6 @@
 user_text = input('Введіть речення або слово, щоб перевірити чи є воно паліндромом: ').replace(" ", "").lower().strip(' !.,')
-check = user_text[::-1]
 
-if user_text == check:
+if user_text == user_text[::-1]:
     print ('Ваше слово є паліндромом!')
 else:
     print('Це слово не є паліндромом')
