dd9005add64ae722a5276d74187c978e94b55910
Karina <kidiaievakarina@gmail.com>
2024-02-03 23:08:55 +0200

    func ex-2 update

--- list.py
+++ list.py
@@ -1,7 +1,9 @@
 def odd_num (a, b):
-    print(f'Непарні числа в діапазоні від {a} до {b}: ')
     for i in range(a, b + 1):
         if i % 2 != 0:
             print (i, end=' ')
 
-odd_num(0, 105)
+a = int(input('Введіть перше число діапазону: '))
+b = int(input('Введіть друге число діапазону: '))
+
+print(f'Непарні числа в діапазоні від {a} до {b}: ', end=''), odd_num(a, b)
